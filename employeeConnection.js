d27fe203f144d982cd49c65ec73aba5cfc7778af
Kristin Saunders <kmsaunders7@gmail.com>
2021-02-08 12:57:23 -0600

    add js, establish connection to mysql

---
+++ employeeConnection.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql')
+const inquirer = require('inquirer')
+const consoleTable = require('console.table')
+
+
+//connection to mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port, if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: '',
+    database: 'employee_db',
+  });
+
+  //establish connection or throw error
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
+  });
+  
