a0c6b3064eabff202a045e2419105158e909682b
Kristin Saunders <kmsaunders7@gmail.com>
2021-02-13 18:18:04 -0600

    update README, attach video demo

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Employee-Tracking
 Manage a company's employees using node, inquirer, and MySQL
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+![Employee Tracker](assets\demo.webm)
+
+## Description
+As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
+
+The command line will prompt a series of options, when choosing the option will take you to the table with the corresponding answer.
+
+
+## Table of Contents
+- [Description](#description)
+- [How to Use](#usage)
+- [Questions](#questions)
+
+
+## HOW TO USE 
+NPM INIT -y
+NPM I --SAVE MYSQL FIGLET INQUIRER
+
+
+## Questions?
+- contact: kmsaunders7@gmail.com
+- GITHUB: kmsaunders7
--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const track = () => {
       type: 'list',
       name: 'choices',
       message: 'Please select option using arrow key.',
-      choices: ['View all employees', 'View Roles', 'View Departments', 'Add employee', 'Remove employee', 'Update employee role', 'End']
+      choices: ['View all employees', 'View Roles', 'View Departments', 'View employees by department', 'View employees by role', 'Add employee', 'End']
     }
   ).then((answer) => {
     //list of switch cases to begin different functions related to the selection
@@ -59,11 +59,14 @@ const track = () => {
       case 'View Roles':
         viewRoles()
         break
+      case 'View employees by department':
+        viewEmpDept()
+        break
+      case 'View employees by role':
+        viewEmpRole()
+        break
       case 'Add employee':
         addEmployee()
-        break
-      case 'Remove employee':
-        deleteEmployee()
         break
       case 'End':
         console.log('Leaving app now, thank you!')
@@ -77,7 +80,7 @@ const track = () => {
 //function for viewing employees
 
 const viewEmployee = () => {
-  connection.query('SELECT * FROM employees', (err, res) => {
+  connection.query('SELECT * FROM employee', (err, res) => {
     if (err) throw err
     //log results in table format
     console.table(res)
@@ -110,8 +113,23 @@ const viewRoles = () => {
   })
 }
 
+// function for viewing employees by department
+const viewEmpDept = () => {
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;", (err, res) => {
+    if (err) throw err
+    console.table(res)
+    track()
+  })
+}
 
-
+// function for viewing employee by role
+const viewEmpRole = () => {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", (err, res) => {
+    if (err) throw err
+    console.table(res)
+    track()
+  })
+};
 
 
 //function for adding an employee
@@ -139,9 +157,10 @@ const addEmployee = () => {
       message: 'What is the I.D of the manager overseeing the employee you want to add?'
     }
   ]).then((answer) => {
-    connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)', [answer.first, answer.last, answer.role, answer.manager], (err, res) => {
+    connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [answer.first, answer.last, answer.role, answer.manager], (err, res) => {
       if (err) throw err
       console.table(res)
+      viewEmployee()
       track()
     })
   })
